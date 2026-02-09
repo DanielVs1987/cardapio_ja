ca77a01c0c6224fa4e19d3345af68e3a21270bfb
DanielVs1987 <161987864+DanielVs1987@users.noreply.github.com>
2026-02-08 23:50:00 -0300

    Add files via upload

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2526364578"
+    serviceWorkerVersion: "217951582"
   }
 });
